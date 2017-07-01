ec2221211f54eed7a8d684f4c0d2a6eec8c79212
Yvette Hernández <yvette.hernandez.alonso@everis.com>
2017-07-01 19:32:32 +0200

    Update - Adding Merchant register and validate username Api methods

--- app/src/main/java/es/uv/uvlive/data/gateway/UVLiveGateway.java
+++ app/src/main/java/es/uv/uvlive/data/gateway/UVLiveGateway.java
@@ -11,6 +11,7 @@ import com.google.gson.GsonBuilder;
 
 import es.uv.uvlive.data.UVCallback;
 import es.uv.uvlive.data.gateway.form.LoginForm;
+import es.uv.uvlive.data.gateway.form.MerchantRegisterForm;
 import es.uv.uvlive.data.gateway.form.MessageForm;
 import es.uv.uvlive.data.gateway.form.MessagesForm;
 import es.uv.uvlive.data.gateway.form.PushTokenForm;
@@ -19,17 +20,20 @@ import es.uv.uvlive.data.gateway.response.ConversationsListResponse;
 import es.uv.uvlive.data.gateway.response.LogListResponse;
 import es.uv.uvlive.data.gateway.response.LoginResponse;
 import es.uv.uvlive.data.gateway.response.MessageListResponse;
+import es.uv.uvlive.data.gateway.response.ValidateMerchantResponse;
 
 public class UVLiveGateway {
 
     private static final String TAG = "UVLiveGateway";
-    private static final String urlLogin = "login";
-    private static final String urlLogout = "logout";
+    private static final String urlLogin = "user/login";
+    private static final String urlLogout = "user/logout";
     private static final String urlConversations ="conversations";
     private static final String urlMessages ="messages";
     private static final String urlLogger ="logger";
     private static final String urlPushToken = "update/push_token";
     private static final String urlSend = "urlSend";
+    private static final String urlRegisterMerchant = "admin/merchant/register";
+    private static final String urlValidateMerchant = "admin/merchantName/exists";
 
     private static String environment = BuildConfig.ENVIRONMENT + "uvlive-api/";
 
@@ -81,6 +85,20 @@ public class UVLiveGateway {
     public void logs(UVCallback<LogListResponse> callback) {
         addRequestToQueue(new GsonRequest<>(environment+ urlLogger,
                 LogListResponse.class,"",callback.Listener,callback.ErrorListener));
+    }
+
+    public void merchantRegister(MerchantRegisterForm form, UVCallback<BaseResponse> callback) {
+        String stringRequest = GSON_CREATOR.toJson(form);
+        GsonRequest<BaseResponse> request = new GsonRequest<>(environment+ urlRegisterMerchant,
+                BaseResponse.class,stringRequest,callback.Listener,callback.ErrorListener);
+        addRequestToQueue(request);
+    }
+
+    public void merchantValidate(MerchantRegisterForm form, UVCallback<ValidateMerchantResponse> callback) {
+        String stringRequest = GSON_CREATOR.toJson(form);
+        GsonRequest<ValidateMerchantResponse> request = new GsonRequest<>(environment+ urlValidateMerchant,
+                ValidateMerchantResponse.class,stringRequest,callback.Listener,callback.ErrorListener);
+        addRequestToQueue(request);
     }
 
     private final <T> void addRequestToQueue(GsonRequest<T> request) {
