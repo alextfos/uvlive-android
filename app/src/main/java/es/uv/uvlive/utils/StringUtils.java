50e0250d816d804ec7ac04ba5fcdba5f58df8fa2
Alejandro Traver Fos <alextfos@gmail.com>
2017-07-12 20:05:55 +0200

    Add generic function to StringUtils

--- app/src/main/java/es/uv/uvlive/utils/StringUtils.java
+++ app/src/main/java/es/uv/uvlive/utils/StringUtils.java
@@ -8,4 +8,7 @@ public class StringUtils {
         return str==null || str.equals("");
     }
 
+    public static boolean equals(String str1, String str2) {
+        return str1 != null && str1.equals(str2);
+    }
 }
